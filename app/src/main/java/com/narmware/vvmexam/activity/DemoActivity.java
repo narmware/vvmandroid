80b3a006b6a6177c5f6f0193d7c55ec1e2dc145e
Narmware Software Solutions <vrushali@narmware.com>
2018-09-12 17:08:57 +0530

    1.Bottom nav activity created. 2.Edit profile UI

---
+++ app/src/main/java/com/narmware/vvmexam/activity/DemoActivity.java
@@ -0,0 +1,23 @@
+package com.narmware.vvmexam.activity;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.narmware.vvmexam.R;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+
+}
